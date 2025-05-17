c472e9e1efc6531aff7a4bc792aac6020698156e
simonangel-fong <simonangelfong@gmail.com>
2025-05-17 19:44:11 -0400

    testing jenkins

--- oracledb/scripts/healthcheck.sh
+++ oracledb/scripts/healthcheck.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 # RETVAL=`sqlplus -silent / as sysdba <<EOF
 # SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF
 # SELECT 'Alive' FROM dual;
 # EXIT;
 # EOF`
 
-RETVAL=$(sqlplus -silent / as sysdba <<EOF
+RETVAL=$(
+  sqlplus -silent / as sysdba <<EOF
 SET PAGESIZE 0 FEEDBACK OFF VERIFY OFF HEADING OFF ECHO OFF
 SELECT open_mode FROM v\\$pdbs WHERE name = 'TORONTO_SHARED_BIKE';
 EXIT;
@@ -12,7 +15,9 @@ EOF
 )
 
 if [ "\${RETVAL}" = "READ WRITE" ]; then
+  echo "Toronto_shared_bike pdb is opened."
   exit 0
 else
+  echo "Toronto_shared_bike pdb is not opened."
   exit 1
 fi
