0b2037dc87529598fdd07f6bfa1eb8a5e9890877
simonangel-fong <simonangelfong@gmail.com>
2025-05-19 17:42:44 -0400

    etl 2021

--- oracledb/scripts/data_analysis/tableau.sql
+++ oracledb/scripts/data_analysis/tableau.sql
@@ -4,36 +4,78 @@ show con_name;
 
 -- query user type
 SELECT 
-    dim_user_type_id            AS dim_user
-    , dim_user_type_name        AS user_nmae
+    dim_user_type_id        AS dim_user
+    , dim_user_type_name    AS user_type
 FROM dw_schema.dim_user_type;
 
--- Aggregate trip count on time dimension and user type dimension
+-- Aggregate trip count on year and user
+SELECT 
+    tm.dim_time_year                AS dim_year
+    , ft.fact_trip_user_type_id     AS dim_user
+    , COUNT(*)                      AS trip_year_count
+FROM dw_schema.fact_trip ft
+JOIN dw_schema.dim_time tm
+ON ft.fact_trip_start_time_id = tm.dim_time_id
+GROUP BY
+    tm.dim_time_year
+    , ft.fact_trip_user_type_id
+ORDER BY
+    tm.dim_time_year
+    , ft.fact_trip_user_type_id;
+
+-- Aggregate trip count on year, month and user
 SELECT 
     tm.dim_time_year                AS dim_year
     , tm.dim_time_month             AS dim_month
-    , tm.dim_time_day               AS dim_day
-    , tm.dim_time_weekday           AS dim_weekday
-    , tm.dim_time_hour              AS dim_hour
     , ft.fact_trip_user_type_id     AS dim_user
-    , COUNT(*)                      AS trip_count
+    , COUNT(*)                      AS trip_month_count
 FROM dw_schema.fact_trip ft
 JOIN dw_schema.dim_time tm
 ON ft.fact_trip_start_time_id = tm.dim_time_id
 GROUP BY
     tm.dim_time_year
     , tm.dim_time_month
-    , tm.dim_time_day
-    , tm.dim_time_weekday
-    , tm.dim_time_hour
     , ft.fact_trip_user_type_id
 ORDER BY
     tm.dim_time_year
     , tm.dim_time_month
-    , tm.dim_time_day
+    , ft.fact_trip_user_type_id;
+
+-- Aggregate trip count on year, hour, and user
+SELECT 
+    tm.dim_time_year                AS dim_year
+    , tm.dim_time_hour              AS dim_hour
+    , ft.fact_trip_user_type_id     AS dim_user
+    , COUNT(*)                      AS trip_hour_count
+FROM dw_schema.fact_trip ft
+JOIN dw_schema.dim_time tm
+ON ft.fact_trip_start_time_id = tm.dim_time_id
+GROUP BY
+    tm.dim_time_year
+    , tm.dim_time_hour
+    , ft.fact_trip_user_type_id
+ORDER BY
+    tm.dim_time_year
     , tm.dim_time_hour
     , ft.fact_trip_user_type_id;
-    
+
+-- Aggregate trip count on year and user
+SELECT 
+    tm.dim_time_year                AS dim_year
+    , ft.fact_trip_user_type_id     AS dim_user
+    , COUNT(*)                      AS trip_year_count
+FROM dw_schema.fact_trip ft
+JOIN dw_schema.dim_time tm
+ON ft.fact_trip_start_time_id = tm.dim_time_id
+GROUP BY
+    tm.dim_time_year
+    , ft.fact_trip_user_type_id
+ORDER BY
+    tm.dim_time_year
+    , ft.fact_trip_user_type_id;
+
+
+----------------------
 -- Aggregate average duration on time dimension and user type dimension
 SELECT 
     tm.dim_time_year                          AS dim_year
@@ -60,42 +102,5 @@ ORDER BY
     , tm.dim_time_hour
     , ft.fact_trip_user_type_id;
     
--- query to count total for each user type
-SELECT
---    /*csv*/
-    tm.dim_time_year                AS dim_year
-    , ft.fact_trip_user_type_id     AS dim_user
-    , COUNT(*)                      AS trip_count
-FROM dw_schema.fact_trip ft
-JOIN dw_schema.dim_time tm
-ON ft.fact_trip_start_time_id = tm.dim_time_id
-GROUP BY
-    tm.dim_time_year
-    , ft.fact_trip_user_type_id
-ORDER BY
-    tm.dim_time_year
-    , ft.fact_trip_user_type_id;
-    
---
----------------------------------------------------
-SELECT 
-    COUNT(*)                      AS trip_count
---    , tm.dim_time_year                AS dim_year
-FROM dw_schema.fact_trip ft
-JOIN dw_schema.dim_time tm
-ON ft.fact_trip_start_time_id = tm.dim_time_id
-GROUP BY
-    tm.dim_time_year
-HAVING tm.dim_time_year = 2019;
-
-
-
-
-
-
-
-
-
-
 
 
--- oracledb/scripts/dba/alter_object.sql
+++ oracledb/scripts/dba/alter_object.sql
@@ -9,3 +9,33 @@ ALTER SESSION SET CONTAINER = toronto_shared_bike;
 SHOW con_name;
 SHOW user;
 
+-- archived log
+SELECT 
+    GROUP#,
+    THREAD#,
+    SEQUENCE#,
+    BYTES / 1024 / 1024 AS SIZE_MB,
+    MEMBERS,
+    STATUS,
+    ARCHIVED
+FROM 
+    V$LOG
+ORDER BY GROUP#;
+
+-- path
+SELECT 
+    GROUP#,
+    MEMBER,
+    TYPE,
+    IS_RECOVERY_DEST_FILE,
+    STATUS
+FROM 
+    V$LOGFILE
+ORDER BY GROUP#, MEMBER;
+
+-- Check current log size
+ALTER DATABASE ADD LOGFILE GROUP 4 ('/opt/oracle/oradata/ORCLCDB/redo04.log') SIZE 500M;
+
+SELECT name, value FROM v$sysstat WHERE name LIKE '%checkpoint%';
+
+
